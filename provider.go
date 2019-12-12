0b986e30e2f657155395fd442e477fd69de044fe
Patrick Stephen <patrick@strongdm.com>
2019-12-12 09:09:52 -0600

    Merge commit
    GitOrigin-RevId: 103f504a9a7f5e733b1ef1405b36a5175ed763f1

--- provider.go
+++ provider.go
@@ -10,11 +10,16 @@ import (
 func Provider() *schema.Provider {
 	return &schema.Provider{
 		Schema: map[string]*schema.Schema{
-			"api_key": {
+			"api_access_key": {
 				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     "",
-				Description: "An authenticated JWT token used to access the StrongDM API.",
+				Required:    true,
+				Description: "A GUID identifying the API key used to authenticate with the StrongDM API.",
+			},
+
+			"api_secret_key": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "A base64 encoded secret key used to authenticate with the StrongDM API.",
 			},
 
 			"host": {
@@ -29,7 +34,7 @@ func Provider() *schema.Provider {
 			"sdm_role": resourceRole(),
 		},
 		ConfigureFunc: func(d *schema.ResourceData) (interface{}, error) {
-			client, err := apiV1.New(d.Get("host").(string), d.Get("api_key").(string))
+			client, err := apiV1.New(d.Get("host").(string), d.Get("api_access_key").(string), d.Get("api_secret_key").(string))
 			if err != nil {
 				return nil, fmt.Errorf("cannot dial API server: %w", err)
 			}
