614e185829ae4c2e888b214fa73250e535ae4808
Evan Todd <evan@etodd.io>
2021-09-09 14:51:20 -0700

    bump terraform version and add release notes
    GitOrigin-RevId: 0505a1a31c7178cee857563ab320de89b19daf34

--- sdm/provider.go
+++ sdm/provider.go
@@ -11,7 +11,7 @@ import (
 	sdm "github.com/strongdm/terraform-provider-sdm/sdm/internal/sdk"
 )
 
-const userAgent = "terraform-provider-sdm/1.0.27"
+const userAgent = "terraform-provider-sdm/1.0.28"
 
 // Provider returns a terraform.ResourceProvider.
 func Provider() terraform.ResourceProvider {
