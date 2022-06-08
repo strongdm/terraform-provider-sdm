721c52247ac1d6ad9140ebec26587eb6684d6731
JP Fielding <jp.fielding@strongdm.com>
2022-06-08 21:10:09 +0000

    Add terraform tests for remote identity datasource
    GitOrigin-RevId: bceca5a33447030c9689886908d1e5e8b19664e7

--- sdm/data_source_remote_identity_group_test.go
+++ sdm/data_source_remote_identity_group_test.go
@@ -1,16 +1,19 @@
 package sdm
 
 import (
+	"context"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	sdm "github.com/strongdm/terraform-provider-sdm/sdm/internal/sdk"
 )
 
 func TestAccSDMRemoteIdentityGroupDataSource_GetByName(t *testing.T) {
 	t.Parallel()
 
-	dsName := randomWithPrefix("test-role-ds")
+	dsName := randomWithPrefix("test-remote-identity-group-ds")
 	name := "default"
 
 	resource.Test(t, resource.TestCase{
@@ -31,4 +34,22 @@ func testAccSDMRemoteIdentityGroupDataSourceGetFilterConfig(dsName, filter strin
 	data "sdm_remote_identity_group" "%s" {
 		name = "%s"
 	}`, dsName, filter)
+}
+
+func getDefaultRemoteIdentityGroup(t *testing.T) *sdm.RemoteIdentityGroup {
+	client, err := preTestClient()
+	if err != nil {
+		t.Fatalf("failed to create test client: %v", err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	filter := "name:default"
+	listResp, err := client.RemoteIdentityGroups().List(ctx, filter)
+	if err != nil {
+		t.Fatalf("list failed: %v", err)
+	}
+	if !listResp.Next() {
+		t.Fatalf("list empty")
+	}
+	return listResp.Value()
 }
---
+++ sdm/data_source_remote_identity_test.go
@@ -0,0 +1,62 @@
+package sdm
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	sdm "github.com/strongdm/terraform-provider-sdm/sdm/internal/sdk"
+)
+
+func TestAccSDMRemoteIdentityDataSource_Get(t *testing.T) {
+	t.Parallel()
+
+	client, err := preTestClient()
+	if err != nil {
+		t.Fatalf("failed to create test client: %v", err)
+	}
+	accounts, err := createUsersWithPrefix("account-get", 1)
+	if err != nil {
+		t.Fatalf("failed to create account in setup: %v", err)
+	}
+	account := accounts[0].(*sdm.User)
+	group := getDefaultRemoteIdentityGroup(t)
+	username := randomWithPrefix("bobby")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	// test setup
+	created, err := client.RemoteIdentities().Create(ctx, &sdm.RemoteIdentity{
+		AccountID:             account.ID,
+		RemoteIdentityGroupID: group.ID,
+		Username:              username,
+	})
+	if err != nil {
+		t.Fatalf("failed to create remote identity in setup: %v", err)
+	}
+	dsName := randomWithPrefix("test-remote-identity-ds")
+	config := fmt.Sprintf(`
+	data "sdm_remote_identity" "%s" {
+		id = "%s"
+		account_id = "%s"
+		remote_identity_group_id = "%s"
+	}`, dsName, created.RemoteIdentity.ID, account.ID, group.ID)
+
+	resource.Test(t, resource.TestCase{
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					// TOOO fix s/remoteidentitys/remoteidentities/ (pkg/api/v1/templates/terraform-provider-sdm/sdm/data_source.go.tpl)
+					resource.TestCheckResourceAttr("data.sdm_remote_identity."+dsName, "remote_identitys.#", "1"),
+					resource.TestCheckResourceAttr("data.sdm_remote_identity."+dsName, "remote_identitys.0.id", created.RemoteIdentity.ID),
+					resource.TestCheckResourceAttr("data.sdm_remote_identity."+dsName, "remote_identitys.0.account_id", created.RemoteIdentity.AccountID),
+					resource.TestCheckResourceAttr("data.sdm_remote_identity."+dsName, "remote_identitys.0.remote_identity_group_id", created.RemoteIdentity.RemoteIdentityGroupID),
+					resource.TestCheckResourceAttr("data.sdm_remote_identity."+dsName, "remote_identitys.0.username", created.RemoteIdentity.Username),
+				),
+			},
+		},
+	})
+}
