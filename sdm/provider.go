cef4fab83068cfd0c3b8d96fa864acf6076d5a0d
Kenny Goodin <kenny.goodin@strongdm.com>
2022-06-14 14:21:27 -0400

    Merge commit
    GitOrigin-RevId: 4ef3294cb4044bc3a9eff44e0cd442191ead2a92

--- sdm/provider.go
+++ sdm/provider.go
@@ -10,7 +10,7 @@ import (
 	sdm "github.com/strongdm/terraform-provider-sdm/sdm/internal/sdk"
 )
 
-const userAgent = "terraform-provider-sdm/2.6.1"
+const userAgent = "terraform-provider-sdm/2.6.0"
 
 // Provider returns a terraform.ResourceProvider.
 func Provider() *schema.Provider {
