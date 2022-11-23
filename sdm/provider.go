e69efb374e9821ac8ea3efbfc5090ac68589acec
Patrick Stephen <patrick@strongdm.com>
2022-11-23 16:33:20 +0000

    regenerate sdks with version bump
    Addresses ENG-42
    
    GitOrigin-RevId: 47f703a8c22490a3cfefa1eea84b363479dccc68

--- sdm/provider.go
+++ sdm/provider.go
@@ -10,7 +10,7 @@ import (
 	sdm "github.com/strongdm/terraform-provider-sdm/sdm/internal/sdk"
 )
 
-const userAgent = "terraform-provider-sdm/3.5.0"
+const userAgent = "terraform-provider-sdm/3.5.1"
 
 // Provider returns a terraform.ResourceProvider.
 func Provider() *schema.Provider {
