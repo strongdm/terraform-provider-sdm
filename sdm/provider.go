6d4c369cd0296ed2d323ddfa85fd6ac26d37227a
SupportSDM <support@strongdm.com>
2025-08-20 13:34:33 +0000

    15.8.0

--- sdm/provider.go
+++ sdm/provider.go
@@ -10,7 +10,7 @@ import (
 	sdm "github.com/strongdm/terraform-provider-sdm/sdm/internal/sdk"
 )
 
-const userAgent = "terraform-provider-sdm/15.7.0"
+const userAgent = "terraform-provider-sdm/15.8.0"
 
 var resourcesMap = map[string]func() *schema.Resource{}
 
